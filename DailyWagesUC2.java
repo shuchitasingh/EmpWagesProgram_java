ca04f0096f65c1c0cd6241a70c8a9355be9c7ed9
shuchitasingh <singhshuchita5@gmail.com>
2021-04-05 10:46:28 +0530

    Added:empWages

---
+++ DailyWagesUC2.java
@@ -0,0 +1,16 @@
+public class DailyWagesUC2 {
+public static void main (String[] args){
+int_ISPRESENT=1;
+int_WAGE_PER_HR=20;
+int_ISFULLTIME=8;
+double empCheck = Math.floor(Math.random()*10) % 2;
+int empHrs = 8;
+if ( ISPRESENT==empcheck)
+   empHrs=8;
+        
+else if(WAGE_PER_HR==empCheck)
+       empHrs=4;
+else
+        System.out.println("Daily Wage of an Fulltime Employee is"+Math.multiplyExact(empRatePerHr,empHrs));
+  }
+}
